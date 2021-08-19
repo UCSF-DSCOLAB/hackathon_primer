c98b545fbfcd65517e64bd6d21050f0bda856818
raybueno <rbueno.phd@gmail.com>
2021-08-19 13:56:06 -0700

    Update start script for both containers.

--- python-container/start.sh
+++ python-container/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo Set your workding directory. This is the directory where all your work will be saved. Please enter full path of directory.
+echo Set your workding directory. This is the directory where all your data lives and where all files/code will be saved. \
+Please enter full path of directory.
 read dir
 docker run -it -p 8888:8888 -v $dir:/home/data drbueno/single-cell-nb:latest
 
--- rstudio-container/start.sh
+++ rstudio-container/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo Set your workding directory. This is the directory where all your work will be saved. Please enter full path of directory.
+echo Set your workding directory. This is the directory where all your data lives and where all files/code will be saved. \
+Please enter full path.
 read dir
 
-docker run -p 8787:8787 -e PASSWORD=@hackathon2021 -v $dir:/home drbueno/rstudio-single-cell
+docker run -d -p 8787:8787 -e PASSWORD=@hackathon2021 -v $dir:/home drbueno/rstudio-single-cell
